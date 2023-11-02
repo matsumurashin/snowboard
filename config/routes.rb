4db7397ed10c1230451da01825231c716b201af2
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-11-02 06:23:09 +0000

    add.model,public/controllers&views,admin/controllers&views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,43 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/check'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'home/top'
+    get 'home/about'
+  end
   devise_for :customers, skip: [:password], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
