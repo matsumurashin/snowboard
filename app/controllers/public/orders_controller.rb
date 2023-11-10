585048c4e208775b8608a6a340d61c7865265cb4
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-11-10 03:03:44 +0000

    作成中:public/orders_controller.rb,models/order.rb,public/orders/new.html.erb

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,14 @@
 class Public::OrdersController < ApplicationController
   def new
+   @order = Order.new
+   @customer = current_customer
+   @shipping_address = current_customer.shipping_address
+  end
+
+  def create
+  end
+
+  def confirm
   end
 
   def complete
@@ -9,5 +18,15 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(order).permit(:payment_method, :status, :name, :postal_code, :address, :shipping_fee, :amont_billed)
+  end
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:customer_id, :postal_code, :address, :name)
   end
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,7 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :customer
+
+  def shipping_address_code
+    "#{postal_code} #{address} #{name}"
+  end
 end
